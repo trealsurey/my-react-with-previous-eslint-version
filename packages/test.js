c31637fc0bca98e10dcc65550d84417fccc3b955
lucylee <trealsurey@gmail.com>
2024-12-23 19:09:17 +0800

    feat: init rollup

--- packages/test.js
+++ packages/test.js
@@ -2,7 +2,13 @@
  * @Author: lucylee
  * @Date: 2024-12-23 18:36:53
  * @LastEditors: lucylee
- * @LastEditTime: 2024-12-23 18:50:57
+ * @LastEditTime: 2024-12-23 19:04:02
  * @Description:
  */
 // const a = 123;
+
+function test() {
+	console.log('test');
+}
+
+test();
